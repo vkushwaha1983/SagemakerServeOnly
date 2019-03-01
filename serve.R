a7f279eda11589ce6be58fe28455aa3e577031b0
vkushwaha1983 <er.vivekkushwaha@gmail.com>
2019-03-01 11:53:05 +0000

    ok

--- serve.R
+++ serve.R
@@ -7,8 +7,6 @@
 # or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 
-# Bring in library that contains multivariate adaptive regression splines (MARS)
-library(mda)
 
 # Bring in library that allows parsing of JSON training parameters
 
